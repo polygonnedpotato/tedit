2125196dd93cb04dcdc94b0d321e1f278343ba56
elburg the polygonned potato <>
2021-05-24 04:22:52 +0000

    new things

---
+++ script.js
@@ -0,0 +1,10 @@
+var version="0.0.0";document.getElementById('t').innerHTML+=" "+version;
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
