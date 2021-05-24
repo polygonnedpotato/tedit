1cd76f6f5c342e9025bff66ce24864bf346d942d
elburg the polygonned potato <>
2021-05-24 04:44:33 +0000

    v0.0.0 official release

--- script.js
+++ script.js
@@ -8,3 +8,8 @@ if ('serviceWorker' in navigator) {
     });
   });
 }
+  const fileSelector = document.getElementById('file-selector');
+  fileSelector.addEventListener('change', (event) => {
+    const fileList = event.target.files;
+    console.log(fileList);
+  });
